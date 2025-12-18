15114ac44b330551556a7d4c196d80920cdbebe9
aamandsahu2-code <aamandsahu2@gmail.com>
2025-12-18 23:33:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Agar tumhe koi options chahiye hon to yaha add karo, for example:
-  // reactStrictMode: true,
-  // images: {
-  //   remotePatterns: [
-  //     { protocol: "https", hostname: "images.unsplash.com" },
-  //   ],
-  // },
+  experimental: {
+    appDir: true,
+  },
 }
 
 export default nextConfig
