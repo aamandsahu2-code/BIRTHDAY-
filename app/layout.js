828345535fc3a0a291fbc3cafc03f5324b424e5c
aamandsahu2-code <aamandsahu2@gmail.com>
2025-12-19 16:38:31 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,35 @@
-import { Kalam } from "next/font/google"
+// app/layout.js
 import "./globals.css"
 
-const kalam = Kalam({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"],
-})
-
 export const metadata = {
-  title: "Happy Birthday Princess!",
-  description: "Princess 19th Birthday Surprise 🎂",
+  title: "Happy Birthday",
+  description: "Special birthday surprise",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${kalam.className} antialiased bg-black select-none`}>
+      <body>
+        {/* background layers */}
+        <div className="background-flowers" />
+
+        {/* animated butterflies */}
+        <div className="butterfly-wrap bw1">
+          <div className="butterfly">
+            <span className="wing left" />
+            <span className="body" />
+            <span className="wing right" />
+          </div>
+        </div>
+
+        <div className="butterfly-wrap bw2">
+          <div className="butterfly">
+            <span className="wing left" />
+            <span className="body" />
+            <span className="wing right" />
+          </div>
+        </div>
+
         {children}
       </body>
     </html>
